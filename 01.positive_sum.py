05ce88de7543c83a238a71133fea38263c6b7f21
Dariuszewski <>
2021-03-08 19:34:37 +0100

    add exercise 1

---
+++ 01.positive_sum.py
@@ -0,0 +1,11 @@
+def positive_sum(arr):
+
+    return sum(filter(lambda x: x>0, arr))
+
+positive_sum([-1,2,10,-6])
+
+'''
+https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python
+https://docs.python.org/3/
+https://docs.python.org/3/library/functions.html#filter
+'''
